2d15f06e4c2e61d8fe7b4035e336c6347eefc22c
Maxim Pyatishev <m.pyatishev@dlinksys.ru>
2013-11-03 23:53:53 +0400

    управление Бобом

--- star-assault/src/net/max/starassault/model/Bob.java
+++ star-assault/src/net/max/starassault/model/Bob.java
@@ -9,7 +9,7 @@ public class Bob {
 		IDLE, WALKING, JUMPING, DYING
 	}
 	
-	public static final float SPEED = 2f;
+	public static final float SPEED = 4f;
 	public static final float JUMP_VELOCITY = 1f;
 	public static final float SIZE = 0.5f;
 	
@@ -17,7 +17,7 @@ public class Bob {
 	Vector2 acceleration = new Vector2();
 	Vector2 velocity = new Vector2();
 	Rectangle bounds = new Rectangle();
-	State stat = State.IDLE;
+	State state = State.IDLE;
 	boolean facingLeft = true;
 	
 	public Bob(Vector2 position) {
@@ -32,5 +32,26 @@ public class Bob {
 
 	public Vector2 getPosition() {
 		return position;
+	}
+	
+	public void setState(State newState) {
+		this.state = newState;
+	}
+
+	public void update(float delta) {
+		position.add(velocity.tmp().mul(delta));
+	}
+
+	public Vector2 getAcceleration() {
+		// TODO Auto-generated method stub
+		return acceleration;
+	}
+	
+	public Vector2 getVelocity() {
+		return velocity;
+	}
+
+	public void setFacingLeft(boolean b) {
+		facingLeft = b;
 	}
 }
--- star-assault/src/net/max/starassault/screens/GameScreen.java
+++ star-assault/src/net/max/starassault/screens/GameScreen.java
@@ -3,10 +3,13 @@
  */
 package net.max.starassault.screens;
 
+import net.max.starassault.controller.WorldController;
 import net.max.starassault.model.World;
 import net.max.starassault.view.WorldRenderer;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL10;
 
@@ -14,10 +17,13 @@ import com.badlogic.gdx.graphics.GL10;
  * @author max
  *
  */
-public class GameScreen implements Screen {
+public class GameScreen implements Screen, InputProcessor {
 	
 	private World world;
 	private WorldRenderer renderer;
+	private WorldController controller;
+	
+	private int width, height;
 
 	/* (non-Javadoc)
 	 * @see com.badlogic.gdx.Screen#render(float)
@@ -26,6 +32,8 @@ public class GameScreen implements Screen {
 	public void render(float delta) {
 		Gdx.gl.glClearColor(0.1f, 0.1f, 0.1f, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		
+		controller.update(delta);
 		renderer.render();
 	}
 
@@ -35,6 +43,8 @@ public class GameScreen implements Screen {
 	@Override
 	public void resize(int width, int height) {
 		renderer.setSize(width, height);
+		this.width = width;
+		this.height = height;
 	}
 
 	/* (non-Javadoc)
@@ -44,6 +54,8 @@ public class GameScreen implements Screen {
 	public void show() {
 		world = new World();
 		renderer = new WorldRenderer(world, true);
+		controller = new WorldController(world);
+		Gdx.input.setInputProcessor(this);
 	}
 
 	/* (non-Javadoc)
@@ -51,8 +63,7 @@ public class GameScreen implements Screen {
 	 */
 	@Override
 	public void hide() {
-		// TODO Auto-generated method stub
-
+		Gdx.input.setInputProcessor(null);
 	}
 
 	/* (non-Javadoc)
@@ -78,8 +89,78 @@ public class GameScreen implements Screen {
 	 */
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
-
+		Gdx.input.setInputProcessor(null);
+	}
+	
+	@Override
+	public boolean keyDown(int keycode) {
+		if (keycode == Keys.LEFT)
+			controller.leftPressed();
+		if (keycode == Keys.RIGHT)
+			controller.rightPressed();
+		if (keycode == Keys.Z)
+			controller.jumpPressed();
+		if (keycode == Keys.X)
+			controller.firePressed();
+		return true;
+	}
+	
+	@Override
+	public boolean keyUp(int keycode) {
+		if (keycode == Keys.LEFT)
+			controller.leftReleased();
+		if (keycode == Keys.RIGHT)
+			controller.rightReleased();
+		if (keycode == Keys.Z)
+			controller.jumpReleased();
+		if (keycode == Keys.X)
+			controller.fireReleased();
+		return true;
+	}
+	
+	@Override
+	public boolean keyTyped(char character) {
+		return false;
+	}
+	
+	@Override
+	public boolean touchDown(int x, int y, int pointer, int button) {
+		if (x < width / 2 && y > height / 2) {
+			controller.leftPressed();
+		}
+		if (x > width / 2 && y > height / 2) {
+			controller.rightPressed();
+		}
+		return true;
+	}
+	
+	@Override
+	public boolean touchUp(int x, int y, int pointer, int button) {
+		if (x < width / 2 && y > height / 2) {
+			controller.leftReleased();
+		}
+		if (x > width / 2 && y > height / 2) {
+			controller.rightReleased();
+		}
+		return true;
 	}
 
+	@Override
+	public boolean touchDragged(int x, int y, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	@Override
+	public boolean mouseMoved(int x, int y) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
 }
